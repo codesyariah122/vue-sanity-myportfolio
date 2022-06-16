7a4335ce6d1bcdb6c248473490ed67a086e3967e
codesyariah122 <pujiermanto@gmail.com>
2022-06-16 17:32:30 +0700

    update style

--- src/components/Home/Person.vue
+++ src/components/Home/Person.vue
@@ -13,9 +13,17 @@
 			<div class="col-md-8 col-lg-8">
 				<div class="d-flex flex-column">
 					<div class="d-flex flex-row justify-content-between align-items-center p-5 bg-dark text-white">
-						<h3 class="display-5 profile-name">
-							{{person.name}}
-						</h3>
+						<div class="row">
+							<div class="col-md-12">								
+								<h3 class="display-5 profile-name">
+									{{person.name}}
+								</h3>
+							</div>
+							<div class="col-md-12">
+								{{person.jobdesk}}
+							</div>
+						</div>
+
 						<div class="social__link">
 							<ul class="list-group list-group-horizontal">
 								<li class="list-group-item">
@@ -47,15 +55,15 @@
 						</div>
 					</div>
 					
-					<div class="p-3 bg-black text-white">
-						<h6>{{person.jobdesk}}</h6>
+					<div class="text-capitalize p-3 bg-black text-white mt-5">
+						<h5>tools or programming languages ​​that I have used</h5>
 					</div>
 
-					<div class="d-flex flex-row text-white">
-						<div class="col-lg-12">
-							<div v-for="(key, index) in skills" class="row justify-content-start mb-2">
-								<div class="col-md-2">
-									{{key.title}}
+					<div class="row text-white mt-2">
+						<div v-for="(key, index) in skills" class="col-md-4">
+							<div class="row justify-content-start mb-3 key-block">
+								<div class="col-md-4">
+									<img :src="imageUrlFor(key.mainImage)" style="width:33px;">
 								</div>
 								<div class="col-md-8">
 									<div class="progress">
